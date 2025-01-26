a3649b97974873d2f6a6631f1a7875047949c0b1
Sachein17 <sacheinn17@gmail.com>
2025-01-26 21:46:02 +0530

    Updated base

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,16 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+	prerender: {
+		handleHttpError: ({ path, referrer, message }) => {
+			// ignore deliberate link to shiny 404 page
+			if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+				return;
+			}
+
+			// otherwise fail the build
+			throw new Error(message);
+		}},
     adapter: adapter()
   }
 };
